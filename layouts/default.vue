d9f1318ae90bda613c03b65ccb6b26ab71311001
Kevin Blair <kevin.blair@emory.edu>
2023-03-29 06:04:44 -0500

    Finish main layout styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,13 @@
 <template>
+    <div id="defaultBg" class="h-screen w-screen fixed bg-no-repeat bg-center bg-cover" />
     <div>
-        Some default layout shared across all pages
+        <!-- <p v-for="i in 100" :key="i">Some default layout shared across all pages</p> -->
         <slot />
     </div>
 </template>
+
+<style scoped lang="css">
+#defaultBg {
+    background-image: url("/rose_pink_bg.svg");
+}
+</style>
