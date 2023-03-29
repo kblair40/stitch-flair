280a322d37ca1a7ae963cf3c9441c3595e6c90cb
Kevin Blair <kevin.blair@emory.edu>
2023-03-29 18:41:23 -0500

    Add Poppins font to nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  head: {
+    link: [
+      {
+        rel: "preconnect",
+        href: "https://fonts.googleapis.com",
+      },
+      {
+        rel: "preconnect",
+        href: "https://fonts.gstatic.com",
+        // crossOriginIsolated,
+      },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500&display=swap",
+      },
+    ],
+  },
   modules: ["@nuxtjs/tailwindcss"],
-  tailwindcss: { /* Options */ }
+  tailwindcss: {
+    // add '~tailwind.config` alias
+    exposeConfig: true,
+  },
 });
