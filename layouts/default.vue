6c5bd14e4b20ab3687e8cad696bffbb37aec21f4
Kevin Blair <kevin.blair@emory.edu>
2023-03-29 17:03:52 -0500

    Add logo

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,9 @@
 <template>
     <div id="defaultBg" class="h-screen w-screen fixed bg-no-repeat bg-center bg-cover" />
+    <div class="fixed top-4 left-4 border">
+        <img src="/logo.svg" class="w-40 h-40" />
+    </div>
+
     <div>
         <!-- <p v-for="i in 100" :key="i">Some default layout shared across all pages</p> -->
         <slot />
