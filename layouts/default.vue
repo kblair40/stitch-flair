47620f39b6e493fea4814879d6418debca1d1b5f
Kevin Blair <kevin.blair@emory.edu>
2023-04-15 14:00:56 -0500

    Add default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,10 @@
+<script lang="ts">
+
+</script>
+
 <template>
     <div id="defaultBg" class="h-screen w-screen fixed bg-no-repeat bg-center bg-cover" />
-    <div class="fixed top-0 left-0 pl-8 pr-16 pt-4 flex justify-between items-start w-screen">
-        <img src="/logo.svg" class="w-40 h-40" />
-
-        <div class="flex items-center space-x-8 pt-4">
-            <p>Home</p>
-            <p>About</p>
-        </div>
-    </div>
+    <Nav />
 
     <div>
         <!-- <p v-for="i in 100" :key="i">Some default layout shared across all pages</p> -->
@@ -19,8 +16,9 @@
 html {
     font-family: Poppins;
 }
+
 #defaultBg {
     background-image: url("/rose_pink_bg.svg");
-    opacity: .4;
+    opacity: .6;
 }
 </style>
