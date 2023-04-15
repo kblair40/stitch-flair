ba38d5b3320cef4012f0802340cd5999141b896b
Kevin Blair <kevin.blair@emory.edu>
2023-04-15 14:12:06 -0500

    Add HamburgerButton for base/sm size screen nav

--- components/Nav.vue
+++ components/Nav.vue
@@ -12,11 +12,13 @@ const logoClasses = [
 
 <template>
     <div :class="navBorder" class="fixed top-0 left-0 pl-8 pr-16 pt-4 flex justify-between items-start w-screen">
-        <img :class="logoClasses" src="/logo2.svg" class="w-60 h-60" />
+        <img :class="logoClasses" src="/logo2.svg" />
 
-        <div class="flex items-center space-x-8 pt-4">
+        <div class="hidden md:flex items-center space-x-8 pt-4">
             <p>Home</p>
             <p>Contact</p>
         </div>
+
+        <NavHamburger />
     </div>
 </template>
---
+++ components/NavHamburger.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+const wrapperClasses = [
+    // 'border border-red-500',
+    'w-12 h-10',
+    'absolute top-6 right-6',
+    'md:hidden',
+    'flex flex-col items-center justify-evenly',
+    'cursor-pointer'
+]
+const lineClasses = [
+    'h-px w-full bg-slate-900'
+]
+</script>
+
+<template>
+    <div :class="wrapperClasses">
+        <span :class="lineClasses" />
+        <span :class="lineClasses" />
+        <span :class="lineClasses" />
+    </div>
+</template>
