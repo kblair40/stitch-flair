2f4491080bbdef54cfe5a02c4fe839f77832f8c7
Kevin Blair <kevin.blair@emory.edu>
2023-04-15 14:00:42 -0500

    Add separate Nav component and pages for Home and Contact

---
+++ components/Nav.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+
+// const navBorder = "border border-blue-500"
+const navBorder = ""
+
+const logoClasses = [
+    "w-36 h-36",
+    "sm:w-48 sm:h-48",
+    "md:w-60 md:h-60",
+]
+</script>
+
+<template>
+    <div :class="navBorder" class="fixed top-0 left-0 pl-8 pr-16 pt-4 flex justify-between items-start w-screen">
+        <img :class="logoClasses" src="/logo2.svg" class="w-60 h-60" />
+
+        <div class="flex items-center space-x-8 pt-4">
+            <p>Home</p>
+            <p>Contact</p>
+        </div>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <!-- <div class="text-2xl font-semibold"> -->
+    <!-- Home Page -->
+    <!-- </div> -->
+</template>
