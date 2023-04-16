5867fe622608b4e7f10ccb0a9673141e04b62325
Kevin Blair <kevin.blair@emory.edu>
2023-04-16 12:05:08 -0500

    Add layout for Featured Products section

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,18 @@
 </script>
 
 <template>
-    <h1 class="font-serif tracking-wide text-4xl font-semibold leading-none">
-        Welcome to Stitch Flair!
-    </h1>
+    <div class="pt-4">
+        <h1 class="font-serif tracking-wide font-semibold leading-none text-2xl sm:text-3xl md:text-4xl">
+            Welcome to Stitch Flair!
+        </h1>
 
-    <p class="mt-4 leading-relaxed">
-        Velit enim commodo adipisicing sunt aliquip officia do velit do voluptate ullamco. Elit nisi proident dolor sunt
-        nisi esse sit eu fugiat cillum magna. Ut commodo fugiat quis consectetur culpa deserunt duis et irure magna laborum
-        dolore officia. Mollit veniam in ipsum elit nostrud.
-    </p>
+        <p class="mt-5 leading-relaxed text-sm sm:text-base">
+            Velit enim commodo adipisicing sunt aliquip officia do velit do voluptate ullamco. Elit nisi proident dolor sunt
+            nisi esse sit eu fugiat cillum magna. Ut commodo fugiat quis consectetur culpa deserunt duis et irure magna
+            laborum
+            dolore officia. Mollit veniam in ipsum elit nostrud.
+        </p>
+
+        <h2 class="mt-8 font-medium text-lg sm:text-xl md:text-2xl tracking-wider">FEATURED PRODUCTS</h2>
+    </div>
 </template>
