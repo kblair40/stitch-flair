3b77376bad828995b66faaed6c8005629bb4e4cb
Kevin Blair <kevin.blair@emory.edu>
2023-04-16 16:00:10 -0500

    Add vueuse package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
       ],
     },
   },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt"],
   tailwindcss: {
     exposeConfig: true,
     cssPath: "~/assets/css/tailwind.css",
