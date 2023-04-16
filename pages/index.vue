ff68cffe65638752597f8bff481f58131d8438dc
Kevin Blair <kevin.blair@emory.edu>
2023-04-16 06:58:25 -0500

    Add placeholder text

--- pages/index.vue
+++ pages/index.vue
@@ -6,4 +6,10 @@
     <h1 class="font-serif tracking-wide text-4xl font-semibold leading-none">
         Welcome to Stitch Flair!
     </h1>
+
+    <p class="mt-4 leading-relaxed">
+        Velit enim commodo adipisicing sunt aliquip officia do velit do voluptate ullamco. Elit nisi proident dolor sunt
+        nisi esse sit eu fugiat cillum magna. Ut commodo fugiat quis consectetur culpa deserunt duis et irure magna laborum
+        dolore officia. Mollit veniam in ipsum elit nostrud.
+    </p>
 </template>
