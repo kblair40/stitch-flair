7391ab8e495454fd584aad86eaa70ea1ce03cfde
Kevin Blair <kevin.blair@emory.edu>
2023-04-20 06:20:27 -0500

    Finish form layout

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,25 +1,46 @@
 <script lang="ts" setup>
 // 
 const inputClasses = [
-    'rounded-md focus-visible:outline-none',
+    'rounded-md focus-visible:outline-none px-3',
     'h-10 w-full',
-    'px-3'
+]
+const textareaClasses = [
+    inputClasses[0],
+    'w-full pt-2'
+]
+const buttonClasses = [
+    'border'
 ]
 </script>
 
 <template>
     <div class="flex flex-col space-y-6">
-        <div class="flex space-x-6">
+    <div class="flex space-x-6">
             <FormControl label="Name">
                 <input :class="inputClasses" />
             </FormControl>
-            <input :class="inputClasses" />
-            <input :class="inputClasses" />
+
+            <FormControl label="Email">
+                <input :class="inputClasses" />
+            </FormControl>
+            <!-- <input :class="inputClasses" />
+                    <input :class="inputClasses" /> -->
         </div>
 
-        <div class="flex space-x-6">
-            <input :class="inputClasses" />
-            <input :class="inputClasses" />
+        <div>
+            <FormControl label="Subject">
+                <input :class="inputClasses" />
+            </FormControl>
+        </div>
+
+        <div>
+            <FormControl label="Message">
+                <textarea rows="3" :class="textareaClasses" />
+            </FormControl>
+        </div>
+
+        <div>
+            <button :class="buttonClasses">Send</button>
         </div>
     </div>
 </template>
