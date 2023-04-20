0effc5965819d9ca604831f46c38d3cfa29b38bb
Kevin Blair <kevin.blair@emory.edu>
2023-04-20 06:04:03 -0500

    Add skeleton for FormControl component

---
+++ components/FormControl.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+interface Props {
+    label: string;
+    size: 'sm' | 'md' | 'lg'
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    size: 'md'
+})
+
+const wrapperClasses = [
+    "flex flex-col"
+]
+</script>
+
+<template>
+    <div :class="wrapperClasses">
+        <p>{{ label }}</p>
+
+        <slot></slot>
+    </div>
+</template>
