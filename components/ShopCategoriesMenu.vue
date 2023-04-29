ea87e620fb5018d892714b7fcae83159b3b6bed6
Kevin Blair <kevin.blair@emory.edu>
2023-04-29 06:01:34 -0500

    Add skeleton for ShopCategoriesMenu

---
+++ components/ShopCategoriesMenu.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+    <div class="flex flex-col w-36 h-full">
+        <div v-for="i in 12" :key="i" class="w-full h-12 border items-center justify-center flex">
+            LABEL
+        </div>
+    </div>
+</template>
