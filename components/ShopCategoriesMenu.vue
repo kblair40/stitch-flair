cffa79e166b018b7b2910b45f9223636e47ba078
Kevin Blair <kevin.blair@emory.edu>
2023-04-29 06:13:28 -0500

    Finish md screen classes

--- components/ShopCategoriesMenu.vue
+++ components/ShopCategoriesMenu.vue
@@ -1,10 +1,29 @@
 <script lang="ts" setup>
+import { categories } from '../utils/constants';
+
+const listClasses = [
+    "flex flex-col",
+    "h-full w-60",
+    "border",
+    "border-t"
+]
+
+const itemClasses = [
+    "flex items-center justify-between px-2",
+    // "leading-tight",
+    // "border-l border-r border-b",
+    "w-full h-12",
+    "cursor-pointer",
+    "hover:bg-white/50",
+]
 </script>
 
 <template>
-    <div class="flex flex-col w-36 h-full">
-        <div v-for="i in 12" :key="i" class="w-full h-12 border items-center justify-center flex">
-            LABEL
+    <div :class="listClasses">
+        <div v-for="category in categories" :key="category" :class="itemClasses">
+            <p>{{ category }}</p>
+
+            <p class="ml-4">{{ Math.floor(Math.random() * 26) }}</p>
         </div>
     </div>
 </template>
