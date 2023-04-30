fc668089be8a42b8c659a540f1fd2f7c82d7388a
Kevin Blair <kevin.blair@emory.edu>
2023-04-30 08:42:31 -0500

    Add skeleton for ProductListItem component

---
+++ components/ProductListItem.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+interface Props {
+    title: string;
+    imgUrl: string;
+    price: number;
+}
+defineProps<Props>();
+
+const cardClasses = [
+    // "bg-blue-200", 
+    'z-20',
+    "mb-4 sm:mr-4 p-2",
+    "bg-white",
+    "cursor-pointer group rounded-md",
+]
+const imgWrapperClasses = [
+    "overflow-hidden",
+    "w-full h-auto sm:h-56 lg:h-64",
+]
+const imgClasses = [
+    "object-cover h-full rounded-sm",
+    "group-hover:scale-105",
+    "transition-transform duration-300"
+]
+</script>
+
+<template>
+    <div :class="cardClasses">
+        <div :class="imgWrapperClasses">
+            <img :src="imgUrl" :class="imgClasses" />
+        </div>
+
+        <p class="font-medium mt-2">{{ title }}</p>
+        <p class="font-semibold mt-1">${{ price.toFixed(2) }}</p>
+    </div>
+</template>
