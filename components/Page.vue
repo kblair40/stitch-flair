37b210027e0db088105e95e3582576b1e2fefac4
Kevin Blair <kevin.blair@emory.edu>
2023-05-01 07:09:14 -0500

    Cleanup unused code

--- components/Page.vue
+++ components/Page.vue
@@ -1,30 +1,5 @@
-<script lang="ts" setup>
-// MD
-// header/nav = 256px height
-// +64px (32px above, 32px below) header (h1)
-// +40px header itself
-// = 360px
-
-// 'h-40 sm:h-52 md:h-64'
-// const headerWrapperClasses = [
-//     // "border border-red-400",
-//     "h-24 md:h-28 flex items-center",
-//     "fixed top-40 sm:top-52 md:top-64"
-// ]
-// const headerClasses = [
-//     "font-serif tracking-wide font-semibold leading-none text-3xl md:text-4xl",
-//     "ml-4 md:ml-6 lg:ml-8"
-// ]
-</script>
-
 <template>
     <div class="min-h-screen flex flex-column">
-        <!-- <div :class="headerWrapperClasses">
-            <h1 :class="headerClasses">
-                <slot name="header"></slot>
-            </h1>
-        </div> -->
-
         <div class="main-content">
             <slot></slot>
 
--- components/PageTitle.vue
+++ components/PageTitle.vue
@@ -1,4 +1,9 @@
 <script lang="ts" setup>
+// MD
+// header/nav = 256px height
+// +64px (32px above, 32px below) header (h1)
+// +40px header itself
+// = 360px
 const headerWrapperClasses = [
     // "border border-red-400",
     "h-24 md:h-28 flex items-center",
