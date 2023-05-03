cf01dad0ea4fe5ebc16773a6e36477f260111048
Kevin Blair <kevin.blair@emory.edu>
2023-05-03 06:37:19 -0500

    Re-add intersection observer logic

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,30 +1,32 @@
 <script lang="ts" setup>
-// const isIntersecting = ref(false);
-// const handleObserve = (entries: any[]) => {
-//     entries.forEach(entry => {
-//         if (!entry.isIntersecting) isIntersecting.value = true
-//         else isIntersecting.value = false
-//     })
-// }
+const isIntersecting = ref(false);
+const handleObserve = (entries: any[]) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) isIntersecting.value = true
+        else isIntersecting.value = false
+    })
+}
 
 
-// onMounted(() => {
-//     const el = document.getElementById('intersectionDetect')
-//     const opts = { root: null, threshold: .5 }
-//     const observer = new IntersectionObserver(handleObserve, opts);
+onMounted(() => {
+    const el = document.getElementById('intersectionDetect')
+    const opts = { root: null, threshold: .5 }
+    const observer = new IntersectionObserver(handleObserve, opts);
 
-//     if (el) {
-//         console.log('observing')
-//         observer.observe(el)
-//     }
-// })
+    if (el) {
+        console.log('observing')
+        observer.observe(el)
+    }
+})
+// h-40 sm:h-52 md:h-64
 </script>
 
 <template>
     <div id="defaultBg" class="h-screen w-screen fixed bg-no-repeat bg-center bg-cover" />
     <!-- base: 152px, sm: 210px, md: 258px -->
-    <Nav :isIntersecting="false" />
-    <!-- <Nav :isIntersecting="isIntersecting" /> -->
+    <!-- <Nav :isIntersecting="false" /> -->
+    <Nav :isIntersecting="isIntersecting" />
+    <div class="relative top-40 sm:top-52 md:top-64" id="intersectionDetect" />
 
     <slot></slot>
 
@@ -42,6 +44,13 @@ html {
 #defaultBg {
     z-index: -1;
     background-image: url("/rose_pink_bg.svg");
-    opacity: .7;
+    opacity: 1;
+}
+
+#intersectionDetect {
+    z-index: 1000;
+    background-color: #ccc;
+    height: 1px;
+    width: 100%;
 }
 </style>
