04d38e70bc22326dd6b6083eeae674b9d86f2cfb
Kevin Blair <kblair40@gmail.com>
2023-05-30 05:30:26 -0500

    Flip boolean logic that decides hide/show state of asterisk

--- components/FormControl.vue
+++ components/FormControl.vue
@@ -18,7 +18,7 @@ const labelClasses = ['md', 'lg'].includes(props.size) ? 'mb-1.5' : 'mb-1'
     <div :class="wrapperClasses">
         <div class="border flex space-x-1">
             <p class="font-medium" :class="labelClasses">{{ label }}</p>
-            <p v-if="!required" class="text-red-400">*</p>
+            <p v-if="required" class="text-red-400">*</p>
         </div>
 
         <slot></slot>
