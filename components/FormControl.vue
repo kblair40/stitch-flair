1fc606381a6f70671e2b576e60d668ee6adcf66d
Kevin Blair <kblair40@gmail.com>
2023-05-30 05:29:18 -0500

    Add optional 'required' prop to form control

--- components/FormControl.vue
+++ components/FormControl.vue
@@ -1,25 +1,25 @@
 <script lang="ts" setup>
 interface Props {
     label: string;
-    size: 'sm' | 'md' | 'lg'
+    size: 'sm' | 'md' | 'lg',
+    required: boolean;
 }
 
 const props = withDefaults(defineProps<Props>(), {
-    size: 'md'
+    size: 'md',
+    required: false
 })
 
-const wrapperClasses = [
-    "flex flex-col w-full"
-]
-
-const labelClasses = [
-    ['md', 'lg'].includes(props.size) ? 'mb-1.5' : 'mb-1',
-]
+const wrapperClasses = "flex flex-col w-full"
+const labelClasses = ['md', 'lg'].includes(props.size) ? 'mb-1.5' : 'mb-1'
 </script>
 
 <template>
     <div :class="wrapperClasses">
-        <p class="font-medium" :class="labelClasses">{{ label }}</p>
+        <div class="border flex space-x-1">
+            <p class="font-medium" :class="labelClasses">{{ label }}</p>
+            <p v-if="!required" class="text-red-400">*</p>
+        </div>
 
         <slot></slot>
     </div>
