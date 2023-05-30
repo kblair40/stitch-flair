6c8d5615e2ea7ad5e0a11714ea4cf3ee785f2972
Kevin Blair <kblair40@gmail.com>
2023-05-30 05:23:57 -0500

    Add all required inputs

--- components/Admin/AddProductForm.vue
+++ components/Admin/AddProductForm.vue
@@ -19,9 +19,13 @@ const formClasses = [
 
 const values = reactive({
     name: '',
-    email: '',
-    subject: '',
-    message: ''
+    price: '',
+    description: '',
+    category: '',
+    image_url: '',
+    featured: false,
+    on_sale: false,
+    on_sale_price: '',
 })
 
 const handleSubmit = () => {
@@ -35,6 +39,34 @@ const handleSubmit = () => {
     <div :class="formClasses">
         <FormControl label="Product Title">
             <input v-model="values.name" :class="inputClasses" />
+        </FormControl>
+
+        <FormControl label="Description">
+            <textarea v-model="values.description" :class="inputClasses" />
+        </FormControl>
+
+        <FormControl label="Price">
+            <input v-model="values.price" :class="inputClasses" />
+        </FormControl>
+
+        <FormControl label="Category">
+            <input v-model="values.category" :class="inputClasses" />
+        </FormControl>
+
+        <FormControl label="Image URL">
+            <input v-model="values.image_url" :class="inputClasses" />
+        </FormControl>
+
+        <FormControl label="On Sale?">
+            <input type="checkbox" v-model="values.on_sale" :class="inputClasses" />
+        </FormControl>
+
+        <FormControl label="Sale Price">
+            <input v-model="values.featured" :class="inputClasses" />
+        </FormControl>
+
+        <FormControl label="Featured?">
+            <input type="checkbox" v-model="values.featured" :class="inputClasses" />
         </FormControl>
 
         <button @click="handleSubmit" class="submit-btn" :class="buttonClasses">Send</button>
