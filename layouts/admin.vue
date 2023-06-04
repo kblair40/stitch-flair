a33f2b86fbf507f23d6ceb7ec44b2a094c945f7e
Kevin Blair <kblair40@gmail.com>
2023-06-04 14:51:28 -0500

    Add layout for admin page

---
+++ layouts/admin.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div class="h-screen" id="defaultBg">
+        <slot></slot>
+    </div>
+</template>
+
+<style lang="css">
+html {
+    font-family: Poppins;
+}
+
+#defaultBg {
+    z-index: -1;
+    background-image: url("/rose_pink_bg.svg");
+    opacity: 1;
+}
+</style>
--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,17 +1,26 @@
 <script lang='ts' setup>
-const tabLabels = ['Add Product', 'Edit Product', 'Add Category'];
+const tabLabels = ['Add Product', 'Edit Products', 'Add Category'];
 const activeTab = ref(tabLabels[0])
-</script>
+
+definePageMeta({
+    layout: 'admin'
+})
+</script> 
 
 <template>
-    <Page>
+    <div class="flex flex-col items-center h-screen">
         <!-- <PageTitle>Admin</PageTitle> -->
-        <div class="fixed top-44 sm:top-56 md:top-68 left-1/2 -translate-x-1/2">
-            <Tabs @change="(tab) => activeTab = tab" :tabLabels="tabLabels" />
+        <!-- <div class="pt-2 flex justify-center border w-screen"> -->
+        <div class="h-14">
+            <div class="fixed top-0 pt-2 pb-1 left-1/2 -translate-x-1/2 border w-screen bg-white">
+                <Tabs @change="(tab) => activeTab = tab" :tabLabels="tabLabels" />
+            </div>
         </div>
 
-        <div class="flex flex-col items-center px-3 sm:px-5 md:px-6 mt-12 flex-1">
+        <!-- <div class="flex flex-col items-center px-3 sm:px-5 md:px-6 flex-1 pt-12 border border-red-300"> -->
+        <div class="px-3 sm:px-5 md:px-6 flex-1 pt-16 border border-red-300 flex-1">
             <AdminAddProductForm v-if="activeTab === 'Add Product'" />
+            <ProductListEditable v-else-if="activeTab === 'Edit Products'" />
         </div>
-    </Page>
+    </div>
 </template>
