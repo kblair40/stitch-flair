df16ffe7f96bbdd49eb44b2761081971d4a65e1a
Kevin Blair <kblair40@gmail.com>
2023-06-04 13:45:22 -0500

    Finish default and active tab styles for tabs

--- components/Tabs.vue
+++ components/Tabs.vue
@@ -5,18 +5,26 @@ interface Props {
 }
 const props = defineProps<Props>();
 
+const activeTab = ref(props.defaultTab || props.tabLabels[0])
+
 const tabsClasses = [
-    'w-full flex justify-center'
+    'w-full flex justify-center',
+    'border'
 ]
-const tabClasses = [
-    'border py-3 px-2 flex items-center justify-center h-min'
+const tabInnerClasses = "py-1 px-3 flex items-center justify-center h-min"
+const activeTabClasses = [
+    // 'bg-white',
+    'font-medium',
+    'border-b border-purplegrey'
 ]
 </script>
 
 <template>
     <div :class="tabsClasses">
-        <div v-for="tabLabel in tabLabels" :class="tabClasses">
-            {{ tabLabel }}
+        <div v-for="tabLabel in tabLabels" class="cursor-pointer h-min w-40" @click="activeTab = tabLabel">
+            <div :class="activeTab === tabLabel ? activeTabClasses.concat(tabInnerClasses) : tabInnerClasses">
+                {{ tabLabel }}
+            </div>
         </div>
     </div>
 </template>
--- pages/sandbox.vue
+++ pages/sandbox.vue
@@ -6,8 +6,8 @@
     <Page>
         <PageTitle>Admin</PageTitle>
 
-        <div class="flex justify-center px-3 sm:px-5 md:px-6 pt-8 h-full">
-            <Tabs  :tabLabels="['Add Product', 'Edit Product', 'Add Category']"/>
+        <div class="flex justify-center px-3 sm:px-5 md:px-6 h-full">
+            <Tabs :tabLabels="['Add Product', 'Edit Product', 'Add Category']"/>
         </div>
     </Page>
 </template>
