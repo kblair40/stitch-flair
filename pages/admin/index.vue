0af04dbfc26e162571c938819dfef25ff104beed
Kevin Blair <kblair40@gmail.com>
2023-06-04 14:31:00 -0500

    Finish tabs layout

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,13 +1,17 @@
 <script lang='ts' setup>
-
+const tabLabels = ['Add Product', 'Edit Product', 'Add Category'];
+const activeTab = ref(tabLabels[0])
 </script>
 
 <template>
     <Page>
-        <PageTitle>Admin</PageTitle>
+        <!-- <PageTitle>Admin</PageTitle> -->
+        <div class="fixed top-44 sm:top-56 md:top-68 left-1/2 -translate-x-1/2">
+            <Tabs @change="(tab) => activeTab = tab" :tabLabels="tabLabels" />
+        </div>
 
-        <div class="flex justify-center px-3 sm:px-5 md:px-6 pt-8">
-            <AdminAddProductForm />
+        <div class="flex flex-col items-center px-3 sm:px-5 md:px-6 mt-12 border flex-1">
+            <AdminAddProductForm v-if="activeTab === 'Add Product'" />
         </div>
     </Page>
 </template>
