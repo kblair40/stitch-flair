f4cd48c431d2cc11a5d3312c41ecf263157a64e8
Kevin Blair <kblair40@gmail.com>
2023-06-04 14:55:17 -0500

    Finish layout

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -8,17 +8,14 @@ definePageMeta({
 </script> 
 
 <template>
-    <div class="flex flex-col items-center h-screen">
-        <!-- <PageTitle>Admin</PageTitle> -->
-        <!-- <div class="pt-2 flex justify-center border w-screen"> -->
+    <div class="flex flex-col items-center h-screen overflow-y-auto">
         <div class="h-14">
-            <div class="fixed top-0 pt-2 pb-1 left-1/2 -translate-x-1/2 border w-screen bg-white">
+            <div class="fixed top-0 pt-2 pb-0 left-1/2 -translate-x-1/2 border w-screen bg-white">
                 <Tabs @change="(tab) => activeTab = tab" :tabLabels="tabLabels" />
             </div>
         </div>
 
-        <!-- <div class="flex flex-col items-center px-3 sm:px-5 md:px-6 flex-1 pt-12 border border-red-300"> -->
-        <div class="px-3 sm:px-5 md:px-6 flex-1 pt-16 border border-red-300 flex-1">
+        <div class="px-3 sm:px-5 md:px-6 pt-16 pb-4 flex-1">
             <AdminAddProductForm v-if="activeTab === 'Add Product'" />
             <ProductListEditable v-else-if="activeTab === 'Edit Products'" />
         </div>
