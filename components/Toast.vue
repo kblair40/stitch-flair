927fc0d4c5e84f90d1f76a3fbdd8df8dea132496
Kevin Blair <kblair40@gmail.com>
2023-06-05 08:06:32 -0500

    Fix bg color not applying

--- components/Toast.vue
+++ components/Toast.vue
@@ -3,15 +3,9 @@ interface Props {
     visible: boolean;
 }
 const props = defineProps<Props>();
-const emit = defineEmits(['close']);
 
 const visibleClasses = ['-translate-y-full']
 const notVisibleClasses = ['-translate-y-0']
-// const wrapperClasses = [
-//     'pb-4 fixed top-vh left-1/2 -translate-x-1/2',
-//     'transition-transform ease-in-out duration-300',
-//     `${props.visible ? visibleClasses : notVisibleClasses}`
-// ]
 const wrapperClasses = computed(() => {
     return [
         'pb-4 fixed top-vh left-1/2 -translate-x-1/2',
@@ -21,7 +15,7 @@ const wrapperClasses = computed(() => {
 })
 const textWrapperClasses = [
     // 'border border-emerald-500',
-    'bg-white h-full py-4 px-8 rounded-md shadow-lg bg-emerald-200/50',
+    'h-full py-4 px-8 rounded-md shadow-lg bg-emerald-200/50',
     'flex justify-center items-center'
 ]
 </script>
