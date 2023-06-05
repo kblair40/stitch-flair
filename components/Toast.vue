c18e1d85c7648f0f38b70c811b04a32319b97a04
Kevin Blair <kblair40@gmail.com>
2023-06-05 08:05:17 -0500

    Fix classes not being computed

--- components/Toast.vue
+++ components/Toast.vue
@@ -7,11 +7,18 @@ const emit = defineEmits(['close']);
 
 const visibleClasses = ['-translate-y-full']
 const notVisibleClasses = ['-translate-y-0']
-const wrapperClasses = [
-    'pb-4 fixed top-vh left-1/2 -translate-x-1/2',
-    'transition-transform ease-in-out duration-300',
-    `${props.visible ? visibleClasses : notVisibleClasses}`
-]
+// const wrapperClasses = [
+//     'pb-4 fixed top-vh left-1/2 -translate-x-1/2',
+//     'transition-transform ease-in-out duration-300',
+//     `${props.visible ? visibleClasses : notVisibleClasses}`
+// ]
+const wrapperClasses = computed(() => {
+    return [
+        'pb-4 fixed top-vh left-1/2 -translate-x-1/2',
+        'transition-transform ease-in-out duration-300',
+        `${props.visible ? visibleClasses : notVisibleClasses}`
+    ]
+})
 const textWrapperClasses = [
     // 'border border-emerald-500',
     'bg-white h-full py-4 px-8 rounded-md shadow-lg bg-emerald-200/50',
