844b3cc3cf633bd94be325e77f68409ede93d0c7
Kevin Blair <kblair40@gmail.com>
2023-06-05 07:32:58 -0500

    Add skeleton for toast component

---
+++ components/Toast.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+
+const wrapperClasses = [
+    'fixed -bottom-full'
+]
+const textWrapperClasses = 'h-40 flex justify-center items-center'
+const textClasses = ""
+</script>
+
+<template>
+    <div :class="wrapperClasses">
+        <div :class="textWrapperClasses">
+            <p :class="textClasses">
+                <slot></slot>
+            </p>
+        </div>
+    </div>
+</template>
