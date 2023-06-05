7698d7d5e855b3da7ed151247a4d1ee5f58463a0
Kevin Blair <kblair40@gmail.com>
2023-06-05 08:28:33 -0500

    Add store for admin state

---
+++ store/adminStore.ts
@@ -0,0 +1,37 @@
+import { defineStore } from "pinia";
+
+type ProductInfo = null | { id: number; idx: number };
+
+export const useAdminStore = defineStore("admin", {
+  state: () => ({
+    showConfirmModal: false,
+    productToDelete: null as ProductInfo,
+    deleting: null as ProductInfo,
+  }),
+  actions: {
+    openConfirmModal(product: ProductInfo) {
+      this.productToDelete = product;
+      this.showConfirmModal = true;
+    },
+    closeConfirmModal() {
+      this.showConfirmModal = false;
+    },
+    async deleteProduct() {
+      try {
+        const id = { ...this.productToDelete }["id"];
+        this.deleting = this.productToDelete;
+        const res = await useCustomFetch(`/product/${id}`, {
+          method: "DELETE",
+        });
+        this.productToDelete = null;
+        console.log("\nRes:", res.data);
+        this.deleting = null;
+        return res;
+      } catch (e) {
+        console.log("\nFailed to delete product");
+        this.deleting = null;
+        return false;
+      }
+    },
+  },
+});
