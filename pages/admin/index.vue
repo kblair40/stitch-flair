97c7a58c965e7c2b08a871db3f1cea38340538aa
Kevin Blair <kblair40@gmail.com>
2023-06-06 06:15:03 -0500

    Fix wrapper not allowing full-width layout

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -9,15 +9,27 @@ definePageMeta({
 
 <template>
     <div class="flex flex-col items-center h-screen overflow-y-auto">
-        <div class="h-14">
-            <div class="h-14 fixed top-0 pt-2 pb-0 left-1/2 -translate-x-1/2 border w-screen bg-white">
+        <div class="tabs-container border border-red-200">
+            <div class="tabs-container fixed top-0 pt-2 pb-0 left-1/2 -translate-x-1/2 border w-screen bg-white">
                 <Tabs defaultTab="Edit Products" @change="(tab) => activeTab = tab" :tabLabels="tabLabels" />
             </div>
         </div>
 
-        <div class="px-3 sm:px-5 md:px-6 pt-16 pb-4 flex-1 w-full">
-            <AdminAddProductForm v-if="activeTab === 'Add Product'" />
-            <ProductListEditable v-else-if="activeTab === 'Edit Products'" />
+        <div class="page-container flex-1 w-screen">
+            <div class="px-3 sm:px-5 md:px-6 w-full h-full max-h-full overflow-y-auto">
+                <AdminAddProductForm v-if="activeTab === 'Add Product'" />
+                <ProductListEditable v-else-if="activeTab === 'Edit Products'" />
+            </div>
         </div>
     </div>
 </template>
+
+<style lang="css" scoped>
+.tabs-container {
+    height: 56px;
+}
+
+.page-container {
+    height: calc(100vh - 56px);
+}
+</style>
