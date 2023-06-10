06a17b853fa7859ef12726b605cc4ca5ec573e6e
Kevin Blair <kblair40@gmail.com>
2023-06-10 06:20:14 -0500

    Remove placeholder borders

--- components/ModalConfirm.vue
+++ components/ModalConfirm.vue
@@ -13,9 +13,9 @@ const handleCancel = () => {
 
 const wrapperClasses = computed(() => {
     return [
+        // 'border border-red-500',
         'z-50 shadow-lg fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 bg-white',
         'py-4 px-3 max-w-lg w-4/5 sm:w-5/6 md:w-1/2 transition-opacity',
-        'border border-red-500'
     ]
 
 })
@@ -23,9 +23,9 @@ const btnClasses = [
     'border border-slate-300 px-3 py-1 rounded-md'
 ]
 const overlayClasses = [
+    // 'border border-blue-600',
     'z-50 fixed top-0 bottom-0 left-0 right-0 w-screen h-screen',
-    'bg-slate-300/50',
-    'border border-blue-600'
+    'bg-slate-300/50'
 ]
 </script>
 
