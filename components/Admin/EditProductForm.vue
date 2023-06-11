6929139ad5b164cf4679044af9817ba957480423
Kevin Blair <kblair40@gmail.com>
2023-06-11 15:47:53 -0500

    Add functionality for pre-populating form w/ existing values

--- components/Admin/EditProductForm.vue
+++ components/Admin/EditProductForm.vue
@@ -1,9 +1,101 @@
 <script lang="ts" setup>
+import { FormKitMessages } from '@formkit/vue'
 
+import { useAdminStore } from '~~/store/adminStore';
+import type { Category, Product } from '~~/utils/types';
+
+const store = useAdminStore();
+
+const formRef = ref<HTMLFormElement | null>(null);
+const formValues = ref<Partial<Product>>({
+    name: '',
+    category_id: 1,
+    description: '',
+    price: '',
+    featured: false,
+    on_sale: false,
+    on_sale_price: '',
+    image_url: '',
+    etsy_url: '',
+})
+
+onMounted(() => {
+    const productToEdit = store.productToEdit as Product;
+    console.log("productToEdit:", productToEdit);
+    formValues.value = {
+        ...productToEdit,
+        price: productToEdit.price.slice(1),
+    };
+})
+
+const handleSubmit = async (values: any) => {
+    console.log('Form values:', values)
+    try {
+        const res = await useCustomFetch('/product', {
+            method: 'PATCH',
+            body: values,
+        })
+        console.log('Create Product Res:', res.data, '\n');
+
+    } catch (e) {
+        console.log('Failed to create product:', e)
+    }
+}
+
+const categoryOptions = computed(() => {
+    const { data: categories } = store.categories;
+    if (categories && Array.isArray(categories) && categories.length) {
+        // console.log('Categories:', categories);
+        return categories.map((category: Category) => ({
+            label: category.title,
+            value: category.id,
+        }))
+    }
+
+    return [];
+})
+
+const wrapperClasses = [
+    "px-2 pt-2 pb-1 md:px-4"
+]
+const formClasses = 'flex flex-col w-full space-y-4 max-w-4xl'
+const formSectionClasses = [
+    "flex flex-col space-y-4",
+    // "lg:flex-row lg:space-x-4 lg:space-y-0"
+]
 </script>
 
 <template>
-    <div class="flex flex-col">
-        Form Here
+    <div :class="wrapperClasses">
+        <FormKit v-model="formValues" ref="formRef" :form-class="formClasses" type="form" submit-label="Save"
+            id="product-form" @submit="handleSubmit">
+            <div :class="formSectionClasses">
+                <FormKit name="name" label="Product Name" type="text" validation="required:trim|length:1,32" />
+                <FormKit name="category_id" label="Category" type="select" :options="categoryOptions"
+                    validation="required" />
+            </div>
+
+            <div :class="formSectionClasses">
+                <FormKit name="description" label="Description" type="textarea" rows="3" />
+                <FormKit validation="required" name="price" label="Price" type="number" :step="0.01" />
+            </div>
+
+            <div class="flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0 lg:space-x-8">
+                <div class="flex space-x-4 lg:space-x-8">
+                    <FormKit name="featured" label="Featured?" type="checkbox" />
+                    <FormKit name="on_sale" label="On Sale?" type="checkbox" />
+                </div>
+                <FormKit label="Sale Price" name="on_sale_price" type="number" :step="0.01" />
+            </div>
+
+            <div :class="formSectionClasses">
+                <FormKit validation="required" name="image_url" label="Image URL" type="text" />
+                <FormKit validation="required" name="etsy_url" label="Etsy URL" type="text" />
+            </div>
+
+            <div class="text-center h-2 relative bottom-2">
+                <FormKitMessages :node="formRef?.node" />
+            </div>
+        </FormKit>
     </div>
 </template>
--- components/Admin/EditProductModal.vue
+++ components/Admin/EditProductModal.vue
@@ -8,13 +8,12 @@ const emit = defineEmits(['confirm', 'cancel'])
 const handleConfirm = () => emit('confirm')
 const handleCancel = () => emit('cancel')
 
-const wrapperClasses = computed(() => {
-    return [
-        // 'border border-red-500',
-        'z-50 shadow-lg fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 bg-white',
-        'py-4 px-3 max-w-lg w-4/5 sm:w-5/6 md:w-1/2 transition-opacity',
-    ]
-})
+const wrapperClasses = [
+    // 'border border-red-500',
+    'z-50 shadow-lg fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 bg-white',
+    'py-4 px-3 max-w-xl w-4/5 sm:w-5/6 md:w-3/4 lg:w-2/3 transition-opacity rounded-md',
+    // 'border border-slate-400'
+]
 const btnClasses = 'border border-slate-300 px-3 py-1 rounded-md'
 const overlayClasses = [
     // 'border border-blue-600',
@@ -27,12 +26,12 @@ const overlayClasses = [
     <div v-if="store.showEditProductModal" class="z-50">
         <div :class="overlayClasses" @click="handleCancel" />
         <div :class="wrapperClasses">
-            <h3 class="text-lg font-medium mb-5 z-50">Please confirm you want to edit this product</h3>
+            <AdminEditProductForm />
 
-            <div class="flex space-x-4 w-full justify-end z-50">
+            <!-- <div class="flex space-x-4 w-full justify-end z-50">
                 <button @click="handleCancel" :class="btnClasses">Cancel</button>
                 <button @click="handleConfirm" :class="btnClasses">Confirm</button>
-            </div>
+            </div> -->
         </div>
     </div>
 </template>
