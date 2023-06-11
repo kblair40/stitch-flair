96a18e8f1930015771445832a7302d6036046983
Kevin Blair <kblair40@gmail.com>
2023-06-11 04:39:50 -0500

    Add edit button and refactor class defs

--- components/Admin/EditCategoryList.vue
+++ components/Admin/EditCategoryList.vue
@@ -16,11 +16,9 @@ const handleClickDelete = (id: number, idx: number) => {
     store.openConfirmModal('category', { id, idx })
 }
 
-const iconBtnClasses = [
-    "transition-colors duration-300 bg-red-100 z-10",
-    "hover:bg-red-200 active:bg-red-300 rounded-full border p-1",
-    "h-7 w-7"
-]
+const iconBtnClasses = "transition-colors duration-300 z-10 h-7 w-7 rounded-full border p-1"
+const trashBtnClasses = "bg-red-100 hover:bg-red-200 active:bg-red-300 " + iconBtnClasses
+const editBtnClasses = "bg-gray-50 hover:bg-gray-100 active:bg-gray-200" + iconBtnClasses;
 </script>
 
 <template>
@@ -29,15 +27,20 @@ const iconBtnClasses = [
 
         <div v-if="categories.length" v-for="category, i in categories" class="mb-1">
             <div class="flex space-x-2 items-center">
-                <div class="pr-4">
-                    <button :class="iconBtnClasses" @click="handleClickDelete(category.id, i)">
+                <div class="pr-4 space-x-2">
+                    <button :class="trashBtnClasses" @click="handleClickDelete(category.id, i)">
                         <img v-if="store.deleting && store.deleting.id === category.id" class="animate-spin"
                             src="/icons/loading.svg" />
                         <img v-else class="scale-90" src="/icons/trash.svg" />
                     </button>
+
+                    <button :class="editBtnClasses" @click="handleClickDelete(category.id, i)">
+                        <img v-if="store.deleting && store.deleting.id === category.id" class="animate-spin"
+                            src="/icons/loading.svg" />
+                        <img v-else class="scale-90" src="/icons/edit.svg" />
+                    </button>
                 </div>
 
-                <!-- <p class="font-medium">Category:</p> -->
                 <p class="font-medium">{{ category.title }}</p>
             </div>
         </div>
