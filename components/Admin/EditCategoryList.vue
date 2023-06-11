3493f5e95723b92e960c192aecbc2cc96a5ef5e7
Kevin Blair <kblair40@gmail.com>
2023-06-11 05:31:02 -0500

    Finish update api call

--- components/Admin/EditCategoryList.vue
+++ components/Admin/EditCategoryList.vue
@@ -16,6 +16,12 @@ const handleClickDelete = (id: number, idx: number) => {
     store.openConfirmModal('category', { id, idx })
 }
 
+const handleClickCancelEdit = () => {
+    editing.id = null;
+    editing.title = null;
+    initialEditVal.value = '';
+}
+
 type EditInfo = { id: null | number, title: null | string }
 const editing: EditInfo = reactive({ id: null, title: null })
 const initialEditVal = ref(''); // holds original category title.  No update will happen if initialEditVal === 'new' saved value.
@@ -31,7 +37,7 @@ const handleClickSave = async () => {
     if (editing.title !== initialEditVal.value) {
         try {
             saving.value = editing.id;
-            const res = await useCustomFetch('/category', {
+            const res = await useCustomFetch(`/category/${editing.id}`, {
                 method: 'PATCH',
                 body: { title: editing.title },
             })
@@ -41,15 +47,16 @@ const handleClickSave = async () => {
         }
     }
 
+    saving.value = null;
     editing.id = null;
     editing.title = null;
     initialEditVal.value = '';
 }
-
+const flexCenter = " flex justify-center items-center"
 const iconBtnClasses = "transition-colors duration-300 z-10 rounded-full border p-1"
 const trashBtnClasses = "bg-red-100 hover:bg-red-200 active:bg-red-300 h-7 w-7 " + iconBtnClasses
 const editBtnClasses = "bg-gray-50 hover:bg-gray-100 active:bg-gray-200 h-7 w-7 " + iconBtnClasses;
-const saveBtnClasses = "bg-gray-50 hover:bg-gray-100 active:bg-gray-200 h-10 w-10 " + iconBtnClasses;
+const saveBtnClasses = "bg-gray-50 hover:bg-gray-100 active:bg-gray-200 h-11 w-11 " + iconBtnClasses + flexCenter;
 </script>
 
 <template>
@@ -57,8 +64,8 @@ const saveBtnClasses = "bg-gray-50 hover:bg-gray-100 active:bg-gray-200 h-10 w-1
         <h3 class="font-semibold text-xl mb-3">All Categories</h3>
 
         <div v-if="categories.length" v-for="category, i in categories" class="mb-1">
-            <div class="flex space-x-2 items-center border">
-                <div class="pr-4 space-x-2">
+            <div class="flex space-x-2 items-center border h-16">
+                <div class="pr-4 space-x-2 min-w-max">
                     <button :disabled="!!editing.id" :class="trashBtnClasses" @click="handleClickDelete(category.id, i)">
                         <img v-if="store.deleting && store.deleting.id === category.id" class="animate-spin"
                             src="/icons/loading.svg" />
@@ -71,18 +78,19 @@ const saveBtnClasses = "bg-gray-50 hover:bg-gray-100 active:bg-gray-200 h-10 w-1
                     </button>
                 </div>
 
-                <!-- <div class="flex space-x-2 items-end border border-emerald-200">
-                    <FormKit :disabled="true" type="text" v-model="editing.title"  />
-                    <button :class="editBtnClasses" @click="handleClickEdit(category.id, category.title)">
-                        <img class="scale-90" src="/icons/save.svg" />
-                    </button>
-                </div> -->
                 <p v-if="!editing.id || editing.id !== category.id" class="font-medium">{{ category.title }}</p>
-                <div v-else class="flex space-x-2 items-end border border-emerald-200 pt-1">
-                    <FormKit type="text" v-model="editing.title" />
-                    <button :class="saveBtnClasses" @click="handleClickSave">
+                <div v-else class="flex space-x-2 items-center pt-1">
+                    <div class="w-max">
+                        <FormKit type="text" v-model="editing.title" />
+                    </div>
+
+                    <button :disabled="initialEditVal === editing.title" :class="saveBtnClasses" @click="handleClickSave">
                         <img v-if="!saving || saving !== category.id" class="scale-90" src="/icons/save.svg" />
                         <img v-else class="animate-spin" src="/icons/loading.svg" />
+                    </button>
+
+                    <button :class="saveBtnClasses" @click="handleClickCancelEdit">
+                        <img class="scale-90" src="/icons/close.svg" />
                     </button>
                 </div>
             </div>
