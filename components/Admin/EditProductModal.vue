f07ad5960bccaef3112f109ded528b78d7c03f2f
Kevin Blair <kblair40@gmail.com>
2023-06-14 06:13:32 -0500

    Add close button

--- components/Admin/EditProductModal.vue
+++ components/Admin/EditProductModal.vue
@@ -10,21 +10,28 @@ const handleClose = () => emit('close')
 const wrapperClasses = [
     // 'border border-red-500',
     'z-50 shadow-lg fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 bg-white',
-    'py-4 px-3 max-w-xl w-4/5 sm:w-5/6 md:w-3/4 lg:w-2/3 transition-opacity rounded-md',
-    // 'border border-slate-400'
+    'max-w-xl w-4/5 sm:w-5/6 md:w-3/4 lg:w-2/3 transition-opacity rounded-md',
 ]
-const overlayClasses = [
-    // 'border border-blue-600',
-    'z-50 fixed top-0 bottom-0 left-0 right-0 w-screen h-screen',
-    'bg-slate-300/50'
+const innerWrapperClasses = 'relative h-full w-full pt-8 pb-4 px-3';
+const overlayClasses = 'z-50 fixed top-0 bottom-0 left-0 right-0 w-screen h-screen'
+const btnClasses = [
+    'rounded-full flex justify-center items-center transition-colors',
+    'absolute top-2 right-2 h-8 w-8',
+    'border border-gray-300 hover:bg-gray-50 active:bg-gray-100'
 ]
 </script>
 
 <template>
     <div v-if="store.showEditProductModal" class="z-50">
-        <div :class="overlayClasses" @click="handleClose" />
+        <div :class="overlayClasses" class="bg-slate-300/50" @click="handleClose" />
         <div :class="wrapperClasses">
-            <AdminEditProductForm @done="handleClose" />
+            <div :class="innerWrapperClasses">
+                <button @click="handleClose" :class="btnClasses">
+                    <img src="/icons/close.svg" />
+                </button>
+
+                <AdminEditProductForm @done;="handleClose" />
+            </div>
         </div>
     </div>
 </template>
