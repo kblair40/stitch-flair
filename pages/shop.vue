c54e088e2f9a27c08167a62f4619603495650f9b
Kevin Blair <kblair40@gmail.com>
2023-06-15 05:06:53 -0500

    Clean up class defs

--- pages/shop.vue
+++ pages/shop.vue
@@ -1,17 +1,14 @@
 <script lang="ts" setup>
 const menuWrapperClasses = [
-    // "border border-purple-700",
+    // "border border-blue-700",
     'hidden sm:block',
-    'w-48 md:w-64 lg:w-76 lg:pl-2',
+    'h-full w-48 md:w-64 lg:w-76 lg:pl-2',
 ]
 const mainContentClasses = [
     // 'border border-red-500'
-    'px-4 md:px-6 w-full',
+    'px-4 md:px-6 w-full h-full',
 ]
-const menuAndContentWrapperClasses = [
-    // "border border-blue-500",
-    "flex w-full",
-]
+const menuAndContentWrapperClasses = "flex w-full h-full"
 </script>
 
 <template>
