9d470748ce6f112850846231dd67455a9a6bcb23
Kevin Blair <kblair40@gmail.com>
2023-06-16 07:58:41 -0500

    Add skeleton for ChipPromo

---
+++ components/ChipPromo.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+withDefaults(defineProps<{ text: string; color?: string }>(), {
+    text: '',
+    color: 'green',
+})
+
+
+</script>
+
+<template>
+    <div class="w-10 h-6 rounded-full">{{ text }}</div>
+</template>
