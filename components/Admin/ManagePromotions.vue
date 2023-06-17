58435455de7e69c973fcb3c79d028d7408377190
Kevin Blair <kblair40@gmail.com>
2023-06-17 07:01:42 -0500

    Remove text transformation before sending req

--- components/Admin/ManagePromotions.vue
+++ components/Admin/ManagePromotions.vue
@@ -2,7 +2,7 @@
 import { reset } from '@formkit/core';
 import { FormKitMessages } from '@formkit/vue'
 
-import { toTitleCase } from '~~/utils/helpers';
+// import { toTitleCase } from '~~/utils/helpers';
 import type { Promotion, PromoColor } from '~~/utils/types';
 import { useAdminStore } from '~~/store/adminStore';
 
@@ -29,28 +29,38 @@ const handleSubmit = async (formValues: any) => {
 
         const res = await useCustomFetch('/promotion', {
             method: 'POST',
-            body: { text: text.toLowerCase().trim(), color },
+            body: { text: text.trim(), color },
         })
+
         console.log('Create Promotion Res:', res.data.value, '\n');
-        console.log('Create Promotion Text:', res.data.value, '\n');
-        store.addPromotion(res.data.value as Promotion);
-        showSuccessToast.value = true;
-        reset('promo-form');
+        if (res.data.value) {
+            store.addPromotion(res.data.value as Promotion);
+            showSuccessToast.value = true;
+            reset('promo-form');
 
-        setTimeout(() => {
-            showSuccessToast.value = false;
-        }, 3000)
-
+            setTimeout(() => {
+                showSuccessToast.value = false;
+            }, 3000)
+        }
     } catch (e) {
         console.log('Failed to create product:', e)
     }
-    loading.value = false;
+
+    setTimeout(() => {
+        loading.value = false;
+    }, 2000)
 }
 
 const colorOptions = ['green', 'red', 'blue', 'orange', 'purple', 'peach'];
 const formClasses = [
     "w-full flex flex-col space-y-4 items-end",
     "sm:flex-row sm:space-x-4 sm:space-y-0 sm:items-end",
+]
+const deleteBtnClasses = [
+    // 'border border-red-300',
+    'border border-slate-300',
+    'h-5 w-5 rounded-full absolute top-0.5 right-0.5 z-10',
+    'transition-colors bg-white hover:bg-red-50 active:bg-red-100'
 ]
 </script>
 
@@ -61,9 +71,12 @@ const formClasses = [
         <div class="w-full max-w-75 sm:max-w-120 md:max-w-150 mt-6">
             <div class="w-full mb-20">
                 <h3 class="font-semibold text-xl mb-3">All Promos</h3>
-                <div class="flex space-x-4">
-                    <div class="border relative pl-4" v-for="promo in store.promotions.data">
-                        <ChipPromo :text="toTitleCase(promo.text)" :color="promo.color" />
+                <div class="flex flex-wrap border">
+                    <div class="relative pr-6 mr-4 mb-2" v-for="promo in store.promotions.data">
+                        <button :class="deleteBtnClasses">
+                            <img class="scale-90" src="/icons/close.svg" />
+                        </button>
+                        <ChipPromo :text="promo.text" :color="promo.color" />
                     </div>
                 </div>
             </div>
@@ -85,7 +98,7 @@ const formClasses = [
                         </div>
 
                         <div class="w-full sm:w-40">
-                            <FormKit :loading="loading" type="submit">Save</FormKit>
+                            <FormKit :disabled="loading" :loading="loading" type="submit">Save</FormKit>
                         </div>
 
                         <div v-show="false" class="text-center h-2 relative bottom-2">
