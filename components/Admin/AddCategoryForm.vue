3478e2ba80718e5d590aeec05a7c2691594479b0
Kevin Blair <kblair40@gmail.com>
2023-06-19 16:49:52 -0500

    Add error handling for creating categories

--- components/Admin/AddCategoryForm.vue
+++ components/Admin/AddCategoryForm.vue
@@ -9,9 +9,12 @@ defineEmits(['submit'])
 
 const store = useAdminStore();
 
+const errorMsg = ref('');
+const showErrorToast = ref(false);
 const showSuccessToast = ref(false);
 const formRef = ref<HTMLFormElement | null>(null);
 const title = ref({ title: '' });
+
 const handleSubmit = async (formValues: any) => {
     // console.log('formValues:', formValues);
     const { title } = formValues;
@@ -21,14 +24,30 @@ const handleSubmit = async (formValues: any) => {
             method: 'POST',
             body: { title: title.toLowerCase().trim() },
         })
-        console.log('Create Category Res:', res.data.value, '\n');
-        store.addCategory(res.data.value as Category);
-        showSuccessToast.value = true;
-        reset('category-form');
 
-        setTimeout(() => {
-            showSuccessToast.value = false;
-        }, 3000)
+        if (res.data.value) {
+            console.log('Create Category Res:', res.data.value, '\n');
+            store.addCategory(res.data.value as Category);
+            showSuccessToast.value = true;
+            reset('category-form');
+
+            setTimeout(() => {
+                showSuccessToast.value = false;
+            }, 6000)
+        } else {
+            console.log('res:', res);
+            let msg = 'Something went wrong';
+            if (res.error.value && res.error.value.response) {
+                msg = res.error.value.response._data.message
+            }
+            
+            errorMsg.value = msg;
+            showErrorToast.value = true;
+            setTimeout(() => {
+                showErrorToast.value = false;
+                errorMsg.value = '';
+            }, 6000)
+        }
 
     } catch (e) {
         console.log('Failed to create product:', e)
@@ -44,6 +63,7 @@ const formClasses = [
 <template>
     <div>
         <Toast :visible="showSuccessToast">Saved Category</Toast>
+        <Toast :error="true" :visible="showErrorToast">{{ errorMsg }}</Toast>
 
         <FormKit :errors="[]" v-model="title" ref="formRef" id="category-form" @submit="handleSubmit" type="form"
             :actions="false">
