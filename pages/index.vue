d1be19bec4c041dbb69540f4a4bacaafd119903f
Kevin Blair <kblair40@gmail.com>
2023-06-20 06:43:32 -0500

    Add fetch to pg cms for text content

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
-// import { DUMMY_PRODUCT } from '../utils/constants';
+import axios from 'axios';
+
 import { useShopStore } from '~~/store/shopStore';
 import { useCustomFetch } from '~~/composables/useCustomFetch';
 
@@ -7,39 +8,46 @@ const store = useShopStore();
 store.getProducts();
 store.getCategories();
 
-const fetchText = async () => {
-    try {
-        const res = await useCustomFetch('/text')
-        console.log('HOME PAGE TEXT:', res.data.value);
-    } catch (e) {
-        console.log('Failed to fetch homepage text:', e);
-    }
+const showErrorToast = ref(false);
+const errorMsg = ref('Something went wrong')
+
+const handleError = () => {
+    showErrorToast.value = true;
+    setTimeout(() => showErrorToast.value = false, 10000); // 10s
 }
 
-onBeforeMount(() => fetchText())
+const content = ref();;
+
+const { data: text, error } = await useCustomFetch('/text');
+console.log('Text/Error:', { text: toRaw(text.value), error: toRaw(error.value) });
+if (error.value) handleError();
+else content.value = { text: toRaw(text.value) }
+
 
 const featuredProductClasses = [
-    "w-full",
-    "mt-4 flex flex-wrap justify-center",
+    "w-full mt-4 flex flex-wrap justify-center",
     "space-y-4 md:space-y-0 md:space-x-4"
 ]
-// const products = [DUMMY_PRODUCT, DUMMY_PRODUCT, DUMMY_PRODUCT, DUMMY_PRODUCT];
 </script>
 
 <template>
     <Page>
-        <PageTitle>Welcome to Stitch Flair!</PageTitle>
+        <div class="h-10 mb-5">
+            <PageTitle v-show="content && !!content.text">Welcome to Stitch Flair!</PageTitle>
+        </div>
 
-        <button class="fixed bottom-2 left-2" @click="fetchText">text</button>
-
-
-        <p class="px-4 md:px-6 lg:px-8 mt-5 leading-relaxed text-sm md:text-mdsm">
-            Velit enim commodo adipisicing sunt aliquip officia do velit do voluptate ullamco. Elit nisi proident dolor
-            sunt
-            nisi esse sit eu fugiat cillum magna. Ut commodo fugiat quis consectetur culpa deserunt duis et irure magna
-            laborum
-            dolore officia. Mollit veniam in ipsum elit nostrud.
-        </p>
+        <div 
+            class="px-4 md:px-6 lg:px-8 mt-5 leading-relaxed text-sm md:text-mdsm"
+            :class="content && !!content.text ? '' : 'min-h-14'"
+        >
+            <p v-show="content && !!content.text">
+                Velit enim commodo adipisicing sunt aliquip officia do velit do voluptate ullamco. Elit nisi proident dolor
+                sunt
+                nisi esse sit eu fugiat cillum magna. Ut commodo fugiat quis consectetur culpa deserunt duis et irure magna
+                laborum
+                dolore officia. Mollit veniam in ipsum elit nostrud.
+            </p>
+        </div>
 
         <div class="flex flex-col items-center">
             <h2 class="text-center font-serif mt-12 font-medium text-xl md:text-2xl tracking-wider">
@@ -49,5 +57,13 @@ const featuredProductClasses = [
                 <ProductCard v-for="product, i in store.featuredProducts" :key="i" v-bind="product" />
             </div>
         </div>
+
+        <Toast :visible="showErrorToast" :error="true">{{ errorMsg }}</Toast>
     </Page>
 </template>
+
+<style scoped lang="css">
+p.home-text {
+    margin: 0;
+}
+</style>
