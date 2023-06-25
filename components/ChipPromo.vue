f2f8a6f0349aba9c3b4683793c930204bfeff574
Kevin Blair <kblair40@gmail.com>
2023-06-25 08:07:25 -0500

    cleanup comments/console.logs

--- components/ChipPromo.vue
+++ components/ChipPromo.vue
@@ -5,12 +5,12 @@ const props = withDefaults(defineProps<{ text: string; color?: PromoColor }>(), 
     text: '',
     color: 'green',
 })
-// const colorOptions = ['green', 'red', 'blue', 'orange', 'purple', 'peach'];
+
 const colorClasses: { [key: string]: string } = {
     green: 'bg-green-300',
     red: 'bg-red-300',
     blue: 'bg-blue-300',
-    orange: 'bg-orange-300', // TODO: Fix this with actual orange color
+    orange: 'bg-orange-300', 
     purple: 'bg-purple-300',
     peach: 'bg-peach-300',
 }
