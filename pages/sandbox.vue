0170393367598a632d8549111a364d72cd706fb2
Kevin Blair <kblair40@gmail.com>
2023-06-27 06:11:15 -0500

    Setup sandbox page for load button testing

--- pages/sandbox.vue
+++ pages/sandbox.vue
@@ -1,13 +1,26 @@
 <script lang="ts" setup>
 
+const loading = ref(false);
+const formValues = ref({ title: '' })
 </script>
 
 <template>
     <Page>
-        <PageTitle>Admin</PageTitle>
+        <!-- <PageTitle>Admin</PageTitle> -->
 
         <div class="flex justify-center px-3 sm:px-5 md:px-6 h-full">
-            <Tabs :tabLabels="['Add Product', 'Edit Product', 'Add Category']"/>
+            <!-- @submit="handleSubmit" -->
+            <FormKit type="form" v-model="formValues" :actions="false">
+                <div class="flex justify-center">
+                    <div class="flex flex-col items-center space-y-6">
+                        <!-- :disabled="loading" -->
+                        <FormKit name="title" type="text" />
+                        <FormKit :loading="loading" type="submit" />
+
+                    </div>
+
+                </div>
+            </FormKit>
         </div>
     </Page>
 </template>
