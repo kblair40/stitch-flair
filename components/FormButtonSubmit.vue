0cffab4d9783a74eb508153c15e1e9679af8988f
Kevin Blair <kblair40@gmail.com>
2023-06-27 10:55:10 -0500

    Add custom submit button

---
+++ components/FormButtonSubmit.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+interface Props {
+    loading: boolean;
+    label: string;
+}
+
+withDefaults(defineProps<Props>(), {
+    loading: false,
+    label: 'Submit'
+})  
+</script>
+
+<template>
+    <FormKit type="submit">
+        <div class="flex justify-center items-center">
+            <span v-if="!loading">{{ label }}</span>
+            <div v-else>
+                <img class="w-6 h-6 animate-spin" src="/icons/loading.svg" />
+            </div>
+        </div>
+    </FormKit>
+</template>
--- pages/sandbox.vue
+++ pages/sandbox.vue
@@ -3,14 +3,33 @@
 const loading = ref(false);
 const formValues = ref({ title: '' })
 
-const handleSubmit = () => {
+const handleSubmit = (formValues: any) => {
+    console.log('Form Values;', formValues);
     loading.value = true;
-    console.log('Loading', loading.value)
+    console.log('\n\n\nLoading', loading.value)
     setTimeout(() => {
         loading.value = false
-        console.warn('Loading', loading.value)
+        console.log('Loading', loading.value)
     }, 2000);
 }
+
+const inputFocusVisibleClasses = 'focus-visible:outline-2 focus-visible:outline-blue-600 focus-visible:outline-offset-2';
+
+`
+
+
+
+
+ 
+`
+const flexClasses = 'inline-flex items-center'
+const loadingClasses = [
+    'formkit-disabled:bg-gray-400 formkit-loading:before:border',
+    'formkit-loading:before:border-2 formkit-loading:before:border-r-transparent formkit-loading:before:border-white',
+    'formkit-loading:before:w-4 formkit-loading:before:h-4',
+    'formkit-loading:before:mr-2 formkit-loading:before:rounded-3xl',
+    'formkit-loading:before:animate-spin'
+]
 
 const bgClasses = 'bg-lightpeach-300 hover:bg-lightpeach-400 active:bg-lightpeach-500';
 const colorClasses = [bgClasses, 'border border-whitepeach-300 transition-colors duration-300'].join(' ')
@@ -23,12 +42,14 @@ const wrapperClasses = [
     '$reset border border-slate-300'
 ].join(' ')
 const inputClasses = [
-    '$reset rounded-md font-semibold',
-    'py-2 px-6',
+    '$reset font-semibold',
+    'py-2 px-6 rounded',
+    flexClasses,
+    loadingClasses,
     colorClasses,
 ].join(' ');
 const labelClasses = [
-    '$reset',
+    '$reset block',
     'border border-red-500'
 ].join(' ')
 
@@ -36,15 +57,15 @@ const labelClasses = [
 // suffixIcon: "$reset block w-4 ml-2 stretch",
 const prefixIconClasses = [
     '$reset',
-    "border border-slate-200",
+    // "border border-slate-200",
     // 'w-4 h-4'
     "block w-4 -ml-2 mr-2 stretch"
 ].join('')
 const suffixIconClasses = [
-    "$reset block w-4 -ml-2 mr-2 stretch",
+    "$reset block w-4 ml-2 stretch",
 ].join(' ')
 
-const buttonClasses = {
+const buttonClasses = ref({
     outer: outerClasses,
     wrapper: wrapperClasses,
     input: inputClasses,
@@ -52,26 +73,28 @@ const buttonClasses = {
     label: labelClasses,
     prefixIcon: prefixIconClasses,
     suffixIcon: suffixIconClasses
-}
+})
+
+console.log('BUTTON CLASSES:', buttonClasses.value)
 </script>
 
 <template>
     <Page>
         <!-- <PageTitle>Admin</PageTitle> -->
 
-        <div class="flex justify-center px-3 sm:px-5 md:px-6 h-full">
-            <!-- @submit="handleSubmit" -->
-            <FormKit type="form" v-model="formValues" :actions="false" @submit="handleSubmit">
-                <div class="flex justify-center">
-                    <div class="flex flex-col items-center space-y-6">
-                        <!-- :disabled="loading" -->
-                        <FormKit name="title" type="text" />
-                        <FormKit :loading="loading" type="submit" :classes="buttonClasses" />
+        <button class="absolute bottom-1 left-1" @click="handleSubmit">dummy submit</button>
+        <FormButtonSubmit label="Submit" :loading="loading" />
 
-                    </div>
-
+        <!-- <div class="flex justify-center px-3 sm:px-5 md:px-6 h-full"> -->
+        <!-- @submit="handleSubmit" -->
+        <!-- <FormKit :data-loading="loading" type="form" v-model="formValues" :actions="false" @submit="handleSubmit">
+            <div class="flex justify-center">
+                <div class="flex flex-col items-center space-y-6">
+                    <FormKit name="title" type="text" />
+                    <FormKit type="submit" :classes="buttonClasses" />
                 </div>
-            </FormKit>
-        </div>
+            </div>
+        </FormKit> -->
+        <!-- </div> -->
     </Page>
 </template>
