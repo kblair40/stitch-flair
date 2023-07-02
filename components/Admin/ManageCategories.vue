0c5f99463613264d3c8b6ca46abc863a7b240099
Kevin Blair <kblair40@gmail.com>
2023-07-02 07:19:47 -0500

    Reverse order of forms'

--- components/Admin/ManageCategories.vue
+++ components/Admin/ManageCategories.vue
@@ -1,22 +1,15 @@
 <script lang="ts" setup>
-const wrapperClasses = [
-    "flex flex-col items-center w-full mt-6",
-    "md:flex-row md:justify-center md:items-start md:px-4 md:space-x-16",
-    "lg:px-8 lg:space-x-32"
-]
-
-const formWrapperClasses = [
-    "mt-10 min-w-70 max-w-90 lg:w-full",
-    "md:mt-0"
-]
+// const wrapperClasses = "flex flex-col items-center w-full mt-6"
+// const formWrapperClasses = "mb-10 min-w-70 max-w-90 lg:w-full md:mt-0"
 </script>
 
 <template>
-    <div :class="wrapperClasses">
-        <AdminEditCategoryList />
-        <div :class="formWrapperClasses">
+    <div class="flex flex-col items-center w-full mt-6">
+        <div class="mb-10 min-w-70 max-w-90 lg:w-full md:mt-0">
             <p class="text-lg font-semibold mb-2">Add Category</p>
             <AdminAddCategoryForm />
         </div>
+
+        <AdminEditCategoryList />
     </div>
 </template>
