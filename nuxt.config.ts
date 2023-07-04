349d9e23ad710406f2890376913eb44d7acfa2ed
Kevin Blair <kblair40@gmail.com>
2023-07-04 11:15:49 -0500

    Add meta for allowance of http requests

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
 export default defineNuxtConfig({
   app: {
     head: {
@@ -15,6 +16,12 @@ export default defineNuxtConfig({
         {
           rel: "stylesheet",
           href: "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500&family=Roboto+Slab:wght@300;400;500;600;700;800&display=swap",
+        },
+      ],
+      meta: [
+        {
+          "http-equiv": "Content-Security-Policy",
+          content: "upgrade-insecure-requests",
         },
       ],
     },
